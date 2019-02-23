20a4d207abf54f498e89b89fc429ee44105543f3
YeabkalA <ywubshit@udallas.edu>
2019-02-23 02:58:52 -0600

    Add login functionality.

--- app/src/main/java/com/example/yeabkalwubshit/marketplace/LoginActivity.java
+++ app/src/main/java/com/example/yeabkalwubshit/marketplace/LoginActivity.java
@@ -46,13 +46,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
@@ -157,6 +150,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         if (mAuthTask != null) {
             return;
         }
+
+        mCreateAccountText.setVisibility(View.GONE);
 
         // Reset errors.
         mEmailView.setError(null);
@@ -325,16 +320,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
+            return AuthFunctions.runAuthentication(mEmail, mPassword);
         }
 
         @Override
@@ -343,10 +329,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                openFeeds();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
+                mCreateAccountText.setVisibility(View.VISIBLE);
             }
         }
 
@@ -362,5 +349,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 CreateAccountActivity.class);
         startActivity(intent);
     }
+
+    private void openFeeds() {
+        Intent intent = new Intent(LoginActivity.this, Feed.class);
+        startActivity(intent);
+    }
+
 }
 
