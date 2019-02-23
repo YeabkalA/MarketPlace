dc1b7c0ec8fa22d14ef7728de067b00e04c56af2
YeabkalA <ywubshit@udallas.edu>
2019-02-23 03:38:31 -0600

    Add base search functionality.

--- app/src/main/java/com/example/yeabkalwubshit/marketplace/Feed.java
+++ app/src/main/java/com/example/yeabkalwubshit/marketplace/Feed.java
@@ -1,7 +1,11 @@
 package com.example.yeabkalwubshit.marketplace;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class Feed extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class Feed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_feed, menu);
+        SearchView searchView = (SearchView)menu.getItem(0).getActionView();
+
+        searchView.setOnQueryTextListener(
+                new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        System.out.println("Tried searching " + query);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        System.out.println("Typing " + newText);
+                        return false;
+                    }
+                }
+        );
+
+        MenuItem item = menu.getItem(1);
+        item.setVisible(true);
+        return true;
+
     }
 }
