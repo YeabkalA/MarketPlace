d1fe26da813bcdd9242dbb0003c2217b252a07b1
YeabkalA <ywubshit@udallas.edu>
2019-03-24 23:46:26 -0500

    Create Bid class.

--- app/src/main/java/com/example/yeabkalwubshit/marketplace/Bid.java
+++ app/src/main/java/com/example/yeabkalwubshit/marketplace/Bid.java
@@ -1,5 +1,96 @@
 package com.example.yeabkalwubshit.marketplace;
 
+import java.util.HashMap;
+
 public class Bid {
-    
+
+    static class Builder {
+
+        Bid bid;
+
+        public Builder() {
+            this.bid = new Bid();
+        }
+
+        public Builder setOwnerId(String id) {
+            this.bid.setOwnerId(id);
+            return this;
+        }
+
+        public Builder setIssuerId(String id) {
+            this.bid.setIssuerId(id);
+            return this;
+        }
+
+        public Builder setItemId(String id) {
+            this.bid.setItemId(id);
+            return this;
+        }
+
+        public Builder setValueInCents(Long val) {
+            this.bid.setValueInCents(val);
+            return this;
+        }
+
+        public Bid build() {
+            return bid;
+        }
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getIssuerId() {
+        return issuerId;
+    }
+
+    public void setIssuerId(String issuerId) {
+        this.issuerId = issuerId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public Long getValueInCents() {
+        return valueInCents;
+    }
+
+    public void setValueInCents(Long valueInCents) {
+        this.valueInCents = valueInCents;
+    }
+
+    private String ownerId;
+    private String issuerId;
+    private String itemId;
+    private Long valueInCents;
+
+    public HashMap<String, Object> createMap() {
+        HashMap<String, Object> ret = new HashMap<>();
+        ret.put("ownerId", ownerId);
+        ret.put("issuerId", issuerId);
+        ret.put("itemId", itemId);
+        ret.put("valueInCents", valueInCents);
+        return ret;
+    }
+
+    public static Bid bidFromMap(HashMap<String, Object> map) {
+        if(map == null) return null;
+        Bid bid = new Builder()
+                .setOwnerId((String) map.get("ownerId"))
+                .setIssuerId((String) map.get("issuerId"))
+                .setItemId((String) map.get("itemId"))
+                .setValueInCents((Long) map.get("valueInCents"))
+                .build();
+        return bid;
+    }
 }
